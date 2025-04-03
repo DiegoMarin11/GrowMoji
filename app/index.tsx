521d20047ae56297666e9ef7dffc678af6a7c1e4
DiegoMarin11 <aether102d@gmial.com>
2025-04-03 15:54:08 -0700

    Pantalla incial en progreso

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet,Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {styles.container}>
+      <Text style ={{
+        fontSize: 24,
+        color:'#fff',
+      }}>
+        Lorem ipssum .....
+        </Text>
+      <Pressable style ={styles.button}>
+
+        <Text style ={{
+          textAlign:'center',  
+          fontSize:16,
+        }}>
+          Click para iniciar
+        </Text>
+      </Pressable>
+
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+container:{
+  flex: 1,
+  backgroundColor: '#015428',
+  justifyContent: "center",
+  alignItems:'center',
+
+  },
+  button:{
+    width: 300,
+    height:30,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+    alignItems:'center',
+    justifyContent: "center",
+    color:'#015428'
+  }
+});
